edbc5f58b5dd3284170032a4122a63f62320a062
Robert Spier <robert.spier@gmail.com>
2020-07-01 23:01:37 +0200

    Initial commit

---
+++ watermark.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+for video in ./files/*.mov; do
+	filename="${video##*/}"
+	ffmpeg -i $video -i ./lib/watermark.png  -filter_complex "overlay=main_w-overlay_w-0+0:0-0+0" ./dist/marked-${filename};
+done;
