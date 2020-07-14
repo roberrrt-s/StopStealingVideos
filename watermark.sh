d27eece8cd0fe326cd7d2c8af429a7be77d823cd
Robert Spier <robert.spier@gmail.com>
2020-07-14 18:04:33 +0200

    Allow whitespace in video names

--- watermark.sh
+++ watermark.sh
@@ -2,5 +2,5 @@
 
 for video in ./files/*.mov; do
 	filename="${video##*/}"
-	ffmpeg -i $video -i ./lib/watermark.png  -filter_complex "overlay=main_w-overlay_w-0+0:0-0+0" ./dist/marked-${filename};
+	ffmpeg -i "$video" -i "./lib/watermark.png"  -filter_complex "overlay=main_w-overlay_w-0+0:0-0+0" "./dist/marked-${filename}";
 done;
