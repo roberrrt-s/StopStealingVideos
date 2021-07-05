a61d638304256b5979ba2dc28f775884903a04c0
Robert Spier <robert.spier@gmail.com>
2021-07-05 12:41:45 +0200

    Add argument for watermark position.

--- watermark.sh
+++ watermark.sh
@@ -2,5 +2,12 @@
 
 for video in ./files/*.mov; do
 	filename="${video##*/}"
-	ffmpeg -i "$video" -i "./lib/watermark.png"  -filter_complex "overlay=main_w-overlay_w-0+0:0-0+0" "./dist/marked-${filename}";
+
+	if [[ $1 == 'topleft' ]] ||  [[ $1 == 'topright' ]] || [[ $1 == 'botleft' ]] ||  [[ $1 == 'botright' ]]; then
+		markimage=$1
+	else
+		markimage='topleft'
+	fi
+
+	ffmpeg -i "$video" -i "./lib/${markimage}.png" -filter_complex "overlay=main_w-overlay_w-0+0:0-0+0" "./dist/marked-${filename}";
 done;
